7eb8847d405abb19386f1b199c782def505641de
tu nombre <ruizdani301@gmail.com>
2022-04-19 16:35:32 -0500

    all

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# create a file using Puppet
+#create a file using Puppet
 file { '/tmp/school':
 ensure  => 'file',
 mode    => '0744',
 owner   => 'www-data'
 group   => 'www-data',
-content => 'I love Puppet'
+content => 'I love Puppet',
 }
