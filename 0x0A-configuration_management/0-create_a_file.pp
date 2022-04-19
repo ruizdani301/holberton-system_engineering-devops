085aafcc59f015aadc4efd055deabf8b42e38547
tu nombre <ruizdani301@gmail.com>
2022-04-19 16:25:28 -0500

    all

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create a file using Puppet
+
+file {'/tmp/school':
+ensure  => file,
+path    => '/tmp/holberton',
+mode    => '0744',
+owner   => 'www-data'
+group   => 'www-data',
+content => 'I love Puppet'
+}
