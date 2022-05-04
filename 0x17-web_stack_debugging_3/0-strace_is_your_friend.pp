299a1e53a42ceba2b3439c20510f29a8d4a0d443
tu nombre <ruizdani301@gmail.com>
2022-05-03 20:16:43 -0500

    se agrgo linea 4

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # error 500
 exec {"sed":
-command => "sed -i 's/class-wp-locale.phpp/ /class-wp-locale.php/g" var/www/html/wp-settings.php ,
+command => "sed -i 's/class-wp-locale.phpp/ /class-wp-locale.php/g" var/www/html/wp-settings.php,
 path => ['/bin', '/usr/bin', '/usr/sbin']
 }
