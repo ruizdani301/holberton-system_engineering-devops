5d7b52d0a4ae84fcb21629e3c32b2a50def5d9b6
tu nombre <ruizdani301@gmail.com>
2022-05-05 17:20:46 -0500

    all

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fixes  nginx
 exec { 'nginx':
-  command => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx && sudo service nginx restart',
-  provider    => shell,
+  command  => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx && sudo service nginx restart',
+  provider => shell,
 }
