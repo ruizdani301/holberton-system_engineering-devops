c9d8c64e03c008ddc1c94fd3b36d0e1d65ff5bd5
tu nombre <ruizdani301@gmail.com>
2022-05-05 18:32:52 -0500

    all

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Fixes  nginx
 exec { 'nginx':
-  command  => "sed -i '$ a\holberton hard nofile 25000\nholberton soft nofile 25000' /etc/security/limits.conf"
+  command  => 'echo -e "\holberton hard nofile 25000\nholberton soft nofile 25000" > /etc/security/limits.conf',
   provider => shell,
 }
