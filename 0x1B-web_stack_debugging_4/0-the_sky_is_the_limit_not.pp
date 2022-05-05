6d3131cd25cb5b3c39eba824053847a0f4f6ff04
tu nombre <ruizdani301@gmail.com>
2022-05-05 16:56:27 -0500

    all

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fixes  nginx
 exec { 'nginx':
-  command => 'sed -i '3i worker_rlimit_nofile 1048576 /etc/nginx/nginx.conf;',
+  command => 'sed -i '3i worker_rlimit_nofile 1048576 /etc/nginx/nginx.conf; sudo service nginx restart',
   path    => ['/bin', '/user/bin', '/usr/sbin']
 }
