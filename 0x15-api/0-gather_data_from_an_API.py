225347ebb739ffd4e542e88d15ddb5cfdac64218
tu nombre <ruizdani301@gmail.com>
2022-05-19 12:25:08 -0500

    all

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,17 +3,19 @@
 import requests
 from sys import argv
 
-i = 0
-j = 0
-user = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
-user_json = user.json()
-task = ""
-todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-for t in todos.json():
-    if t.get('userId') == int(argv[1]):
-        if t['completed']:
-            task += "       " + t['title'] + '\n'
-            i += 1
-        j += 1
-print("Employee {} is done with tasks({}/{}):".format(user_json["name"], i, j))
-print(task, end='')
+if __name__ == "__main__":
+
+    i = 0
+    j = 0
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
+    user_json = user.json()
+    task = ""
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
+            if t['completed']:
+                task += '\t' + t['title'] + '\n'
+                i += 1
+            j += 1
+    print("Employee {} is done with tasks({}/{}):".format(user_json["name"], i, j))
+    print(task, end='')
